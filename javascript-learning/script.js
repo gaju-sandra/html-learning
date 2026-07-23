1aca4116a05d2782e57a9a82dc41489d80b88ebe
Gaju Sandra <gajusandra872@gmail.com>
2026-07-23 15:24:27 +0200

    the start up of java script

--- javascript-learning/script.js
+++ javascript-learning/script.js
@@ -1,1 +1,11 @@
-alert("Welcome to JavaScript!");
+const school = "AUCA";
+
+let firstName = "Sandra";
+
+let age = 22;
+
+alert("Welcome to Student:" + school  );
+
+alert("Student: " + firstName);
+
+alert("Age: " + age);
