2c1d12bb2af0aa1d5b8bc1b0376dc2a6369e9df7
Gaju Sandra <gajusandra872@gmail.com>
2026-07-24 10:26:08 +0200

    the changes

--- javascript-learning/script.js
+++ javascript-learning/script.js
@@ -1,5 +1,15 @@
-const button = document.getElementById("btn");
+// Get the HTML elements
+const input = document.getElementById("studentName");
+const button = document.getElementById("addBtn");
+const message = document.getElementById("message");
 
+// When the button is clicked...
 button.addEventListener("click", function () {
-    alert("You clicked the button!");
+
+    // Get what the user typed
+    const name = input.value;
+
+    // Display a message
+    message.textContent = "Welcome " + name + "!";
+
 });
