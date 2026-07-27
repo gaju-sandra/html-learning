fb696d2c53184615fcfebea0775b34ce7c6f424b
Gaju Sandra <gajusandra872@gmail.com>
2026-07-27 10:15:42 +0200

    the javascript addition

--- css/green/script.js
+++ css/green/script.js
@@ -1,5 +1,8 @@
+
+
 const searchButton = document.getElementById("searchBtn");
 
-searchButton.addEventListener("click", function () {
-    alert("Welcome to greenlife!");
+searchButton.addEventListener("click", function (){
+alert("Thank you for searching something!");
+
 });
