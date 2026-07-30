8d7d9ec081caa32fd24b5da34cf7785cfeafd3ed
Gaju Sandra <gajusandra872@gmail.com>
2026-07-30 11:04:28 +0200

    javascript basics

--- js- basics/index.js
+++ js- basics/index.js
@@ -17,6 +17,34 @@ let name1='Mosh';//String literal
 let age=30;//Number
 let isApproved= false;//Boolean literal
 let firstName= undefined;//(when u don't assign a variable)
-let selectedColor = null;
+let selectedColor = null;// the type is an object
+
 console.log(name1);
+
+//java script is a progamming language that is dynamic
+
+
+//2.REERENCE TYPES
+//object
+
+let person ={
+    //properties
+    name: 'Mosh',
+    age: 30
+};
+console.log(person);
+//ways o accessing a property
+
+//a.dot natation
+person.name= 'john';
+
+//.bracket notation
+person["age"] = 29;
+
+console.log(person.age);
+
+let selection = "age";
+person [selection]= '15';
+console.log(person.age);
+
 
