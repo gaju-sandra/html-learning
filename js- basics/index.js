10d71c6761ac24bbb02acf7511f7f2b22622c6db
Gaju Sandra <gajusandra872@gmail.com>
2026-07-30 10:21:38 +0200

    javascript basics

--- js- basics/index.js
+++ js- basics/index.js
@@ -1,1 +1,11 @@
 console.log('Hello World');
+//variable declaration and the initialization 
+//let: a reserved word 
+let name='Gaju';
+console.log(name);
+
+//const:is used when u dont want the value of a variable to change 
+
+const interestrate=0.3;
+console.log(interestrate);
+
