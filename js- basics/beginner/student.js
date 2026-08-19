40ff6aec0f3a3c3128f1d4b126a9210a14d8f17e
Gaju Sandra <gajusandra872@gmail.com>
2026-08-19 10:30:23 +0200

    the student exercise

--- js- basics/beginner/student.js
+++ js- basics/beginner/student.js
@@ -1,7 +1,24 @@
-let mark = Number(prompt("Enter the student's mark:"));
+let studentName = document.getElementById("studentName");
+let studentMark = document.getElementById("studentMark");
+let addStudent = document.getElementById("addStudentBtn");
+let studentList = document.getElementById("studentlist");
 
-if (mark >= 50) {
-    console.log("Passed");
-} else {
-    console.log("Failed");
-}
+
+addStudent.addEventListener("click", function() {
+
+    let name = studentName.value;
+    let mark = Number(studentMark.value);
+
+    let result;
+
+    if (mark >= 50) {
+        result = "Passed";
+    } else {
+        result = "Failed";
+    }
+
+    studentList.innerHTML += `
+        <p>${name} got ${mark} so the student: ${result}</p>
+    `;
+
+});
