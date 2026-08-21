421b1a814163b77e2081394a3ad91fd74f4fc744
Gaju Sandra <gajusandra872@gmail.com>
2026-08-21 12:09:20 +0200

    learning arrays

--- js- basics/beginner/trial.js
+++ js- basics/beginner/trial.js
@@ -2,6 +2,10 @@ let studentName = document.getElementById("studentName");
 let studentMark = document.getElementById("studentMark");
 let addStudent = document.getElementById("addStudentBtn");
 let studentList = document.getElementById("studentlist");
+let student = [];
+function displaymessage(message){
+        studentList.innerHTML += `<p>${message}</p>`;
+    }
 
 addStudent.addEventListener("click", function() {
 
@@ -9,9 +13,7 @@ addStudent.addEventListener("click", function() {
     let mark = Number(studentMark.value);
     let result;
 
-    function displaymessage(message){
-        studentList.innerHTML += `<p>${message}</p>`;
-    }
+    
 
    
 if(name.trim() ==="" && studentMark.value ===""){
@@ -21,4 +23,20 @@ if(name.trim() ==="" && studentMark.value ===""){
         result =" please fill in the required fields";
         displaymessage(result);
     }
+else if (mark < 0 || mark > 100) {
+        result = "enter a mark between 0 and 100";
+        displaymessage(`Please ${name} ${result}`);
+    } else if (mark >= 50) {
+        result = "Passed";
+        displaymessage(`${name} got ${mark} so the student: ${result}`);
+    } else {
+        result = "Failed";
+        displaymessage(`${name} got ${mark} so the student: ${result}`);
+    }
+
+    if (name.trim() !== "" && studentMark.value !== "" && mark >= 0 && mark <= 100) {
+        student.push({ name: name, mark: mark, result: result });
+        console.log(student);
+    }
+
 });
